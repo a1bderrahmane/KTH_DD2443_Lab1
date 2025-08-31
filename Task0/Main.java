424437b3fea5d976acd47e6587746b0bd9c2da3b
a1bderrahmane <abderrahmaneetudier@gmail.com>
2025-08-31 15:40:08 +0200

    task 1 done

--- Task0/Main.java
+++ Task0/Main.java
@@ -1,7 +1,9 @@
-
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 public class Main {
     public static void main(String[] args) {
-        Runnable1 runnable1 = new Runnable1();
+        Runnable0 runnable1 = new Runnable0();
         Thread[] threads =new Thread[8];
         for (int i = 0; i < 8; i++) {
             threads[i] = new Thread(runnable1, "" + i);
---
+++ Task0/Runnable0.java
@@ -0,0 +1,6 @@
+public class Runnable0 implements Runnable{
+    public void run()
+    {
+        System.out.println("Hello world from thread :"+Thread.currentThread().getName());
+    }
+}
