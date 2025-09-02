83d276465bc02a748e9b2c42cac42df650133e37
a1bderrahmane <abderrahmaneetudier@gmail.com>
2025-09-02 13:08:34 +0200

    Task 4 done i guess

--- Task4/src/CountingSemaphore.java
+++ Task4/src/CountingSemaphore.java
@@ -1,14 +1,31 @@
-public class CountingSemaphore {
+class CountingSemaphore {
+	private  int n;
+	private  int signals;
 
 	public CountingSemaphore(int n) {
-		// TODO
+		this.n = n;
+		signals = 0;
 	}
 
-	public void signal() {
-		// TODO
+	public synchronized void s_wait() throws InterruptedException {
+		n--;
+		// Here making a loop that will constantly verify if the n condition is holding
+		// to avoid spurious wakeup
+		if (n<0)
+		{
+			while(signals==0)
+			{
+				wait();
+			}
+			signals--;
+		}
 	}
 
-	public void s_wait() {
-		// TODO
+	public synchronized void signal() {
+		n++;
+		if (n <= 0) {
+			notify();
+			signals++;
+		}
 	}
 }
